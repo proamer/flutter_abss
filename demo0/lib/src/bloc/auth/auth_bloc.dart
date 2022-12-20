b2b266f2faf6edaf996bc0e9cf103674cf81697f
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-20 12:07:42 +0700

    1234

--- demo0/lib/src/bloc/auth/auth_bloc.dart
+++ demo0/lib/src/bloc/auth/auth_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:demo0/src/constants/network_api.dart';
 import 'package:demo0/src/models/user.dart';
 import 'package:equatable/equatable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -11,11 +12,16 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthState()) {
-    on<AuthEvent_Login>((event, emit) {
+    on<AuthEvent_Login>((event, emit) async {
       final String username = event.payload.username;
       final String password = event.payload.password;
 
       if (username == 'admin' && password == '1234') {
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString(NetworkAPI.token, 'TExkgk0494oksrkf');
+        await prefs.setString(NetworkAPI.username, username);
+
+
       } else {}
     });
   }
