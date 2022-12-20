9eef3d81bef399990d00e0452db5c8ab082b134e
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-20 11:43:51 +0700

    1234

--- demo0/lib/src/bloc/auth/auth_bloc.dart
+++ demo0/lib/src/bloc/auth/auth_bloc.dart
@@ -7,7 +7,7 @@ part 'auth_event.dart';
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(AuthInitial()) {
+  AuthBloc() : super(AuthState()) {
     on<AuthEvent>((event, emit) {
       // TODO: implement event handler
     });
--- demo0/lib/src/bloc/auth/auth_event.dart
+++ demo0/lib/src/bloc/auth/auth_event.dart
@@ -2,4 +2,13 @@ part of 'auth_bloc.dart';
 
 abstract class AuthEvent extends Equatable {
   const AuthEvent();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
+}
+
+
+class AuthEvent_Login extends AuthEvent{
+
 }
