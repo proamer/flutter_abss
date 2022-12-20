5535edd757a2cd7e501e03a5b0d9e685a4b65cce
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-20 10:25:28 +0700

    1234

--- demo0/lib/src/bloc/counter/counter_bloc.dart
+++ demo0/lib/src/bloc/counter/counter_bloc.dart
@@ -7,9 +7,16 @@ part 'counter_event.dart';
 part 'counter_state.dart';
 
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
-  CounterBloc() : super(CounterInitial()) {
-    on<CounterEvent>((event, emit) {
-      // TODO: implement event handler
+  CounterBloc() : super(CounterState(counter: 0)) {
+
+    // Add
+    on<CounterEvent_Add>((event, emit) {
+      emit(state.copyWith(counter: state.counter+1));
+    });
+
+    // Remove
+    on<CounterEvent_Add>((event, emit) {
+      emit(state.copyWith(counter: state.counter-1));
     });
   }
 }
--- demo0/lib/src/bloc/counter/counter_event.dart
+++ demo0/lib/src/bloc/counter/counter_event.dart
@@ -2,4 +2,12 @@ part of 'counter_bloc.dart';
 
 abstract class CounterEvent extends Equatable {
   const CounterEvent();
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
+
+class CounterEvent_Add extends CounterEvent {}
+class CounterEvent_Remove extends CounterEvent {}
+
