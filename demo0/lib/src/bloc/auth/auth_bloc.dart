9bd24a1958cc64788d8495936561604bd9cd15fe
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-20 11:49:42 +0700

    1234

--- demo0/lib/src/bloc/auth/auth_bloc.dart
+++ demo0/lib/src/bloc/auth/auth_bloc.dart
@@ -5,12 +5,17 @@ import 'package:demo0/src/models/user.dart';
 import 'package:equatable/equatable.dart';
 
 part 'auth_event.dart';
+
 part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthState()) {
     on<AuthEvent_Login>((event, emit) {
-      // TODO: implement event handler
+      final String username = event.payload.username;
+      final String password = event.payload.password;
+
+      if (username == 'admin' && password == '1234') {
+      } else {}
     });
   }
 }
