b160bd088550b196455d5a2187d18790dc8152d0
Lek CodeMobiles <chaiyasit.t@gmail.com>
2022-12-20 09:16:26 +0700

    1234

--- demo0/lib/src/pages/login/login_page.dart
+++ demo0/lib/src/pages/login/login_page.dart
@@ -26,55 +26,60 @@ class _LoginPageState extends State<LoginPage> {
         body: Container(
             width: double.infinity,
             height: double.infinity,
-            color: Colors.black,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                _buildBanner(),
-                _buildForm(),
-              ],
+            // color: Colors.black,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  _buildBanner(),
+                  _buildForm(),
+                  SizedBox(height: 30,)
+                ],
+              ),
             )));
   }
 
   _buildBanner() {
     return Padding(
-      padding: const EdgeInsets.only(top: 50.0),
+      padding: const EdgeInsets.only(top: 90.0),
       child: Image.asset(Asset.logoImage),
     );
   }
 
   _buildForm() {
-    return Container(
+    return Card(
+      elevation: 5,
       margin: EdgeInsets.only(top: 50, left: 32, right: 32),
-      padding: EdgeInsets.only(top: 10, left: 32, right: 32),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          TextField(
-            controller: _usernameController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              border: InputBorder.none,
-              hintText: 'codemobiles@gmail.com',
-              labelText: 'Username',
-              icon: Icon(Icons.email),
+      child: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _usernameController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'codemobiles@gmail.com',
+                labelText: 'Username',
+                icon: Icon(Icons.email),
+              ),
             ),
-          ),
-          TextField(
-            obscureText: true,
-            controller: _passwordController,
-            decoration: const InputDecoration(
-              border: InputBorder.none,
-              hintText: 'Enter password',
-              labelText: 'Password',
-              icon: Icon(Icons.password_outlined),
+            TextField(
+              obscureText: true,
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Enter password',
+                labelText: 'Password',
+                icon: Icon(Icons.password_outlined),
+              ),
             ),
-          ),
-          SizedBox(height: 30),
-          ElevatedButton(onPressed: _handleLogin, child: Text("Login")),
-          OutlinedButton(onPressed: _handleRegister, child: Text("Register")),
-          SizedBox(height: 30),
-        ],
+            SizedBox(height: 30),
+            ElevatedButton(onPressed: _handleLogin, child: Text("Login")),
+            OutlinedButton(onPressed: _handleRegister, child: Text("Register")),
+          ],
+        ),
       ),
       color: Colors.white,
     );
